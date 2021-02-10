03c4567fa663b3ef1c7b422da5d62d1e2176e5c6
JupiteRus <atakancigdem7@outlook.com>
2021-02-11 00:02:39 +0300

    .

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,11 +10,22 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductTest();
+            
             //CategoryTest();
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            var result = productManager.GetProductDetails();
 
-
-
+            if (result.Success == true)
+            {
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
         }
 
         private static void CategoryTest()
@@ -23,15 +34,6 @@ namespace ConsoleUI
             foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
-            }
-        }
-
-        private static void ProductTest()
-        {
-            ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetProductDetails())
-            {
-                Console.WriteLine(product.ProductName + " : " + product.CategoryName);
             }
         }
     }
